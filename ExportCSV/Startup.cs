bc3af7e392989d50f424e32ddb22dce84c1b802f
Bharathi <Bharathi@BHARATHIPREM>
2016-12-23 00:17:04 +0000

    Add project files.

---
+++ ExportCSV/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExportCSV.Startup))]
+namespace ExportCSV
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
